5c174a7449ad8b392c8e3dc6a7d972e3ac3a7a93
Ivan <114409228+IvanBondarenkoIT@users.noreply.github.com>
2022-10-14 13:21:35 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
-from turtle import Turtle, Screen
-from random import random
+import turtle as t
+from random import random, choice, randint
 
 
+def random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    return (r, g, b)
 
-tim = Turtle()
+
+def step_forward(tim, distance):
+    tim.pencolor(random_color())
+    tim.forward(distance)
+
+def x_angle_drow():
+    for i in range(3, 11):
+        tim.pencolor(random_color())
+        angle = 360 / i
+        for j in range(i):
+            tim.forward(100)
+            tim.right(angle)
+
+
+def random_move():
+    for _ in range(100):
+        angle = choice(directions)
+        tim.setheading(angle)
+        step_forward(tim, 20)
+
+
+tim = t.Turtle()
+t.colormode(255)
+
+directions = [0, 90, 180, 270]
 tim.shape("turtle")
 tim.color("red")
-print(tim.screen.colormode())
-#tim.pencolor(randint(255),randint(255),randint(255))
-for i in range(3, 11):
-    rgb_color = (random(), random(), random())
-    tim.pencolor(rgb_color)
-    angle = 360 / i
+tim.pensize(5)
+tim.speed(10)
 
-    for j in range(i):
-        tim.forward(100)
-        tim.right(angle)
+x_angle_drow()
+random_move()
 
 
-
-    # tim.penup() if i % 2 else tim.pendown()
-    # tim.forward(10)
-
-
-
-
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
