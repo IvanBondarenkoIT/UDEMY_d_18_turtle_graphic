bf67be1f522593036594ea6fd09de5cceb8cb337
Ivan <114409228+IvanBondarenkoIT@users.noreply.github.com>
2022-10-14 13:55:04 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def random_color():
     return (r, g, b)
 
 
-def step_forward(tim, distance):
+def step_forward(distance):
     tim.pencolor(random_color())
     tim.forward(distance)
 
-def x_angle_drow():
+def draw_x_angle():
     for i in range(3, 11):
         tim.pencolor(random_color())
         angle = 360 / i
@@ -22,11 +22,22 @@ def x_angle_drow():
             tim.right(angle)
 
 
-def random_move():
-    for _ in range(100):
+def draw_random_move(repits: int=100):
+    for _ in range(repits):
         angle = choice(directions)
         tim.setheading(angle)
-        step_forward(tim, 20)
+        step_forward(20)
+
+
+def draw_spirograph(radius: int=80):
+
+    tim.pensize(1)
+    angle = 0
+    while angle < 360:
+        tim.pencolor(random_color())
+        tim.circle(radius)
+        tim.right(5)
+        angle += 5
 
 
 tim = t.Turtle()
@@ -38,8 +49,11 @@ tim.color("red")
 tim.pensize(5)
 tim.speed(10)
 
-x_angle_drow()
-random_move()
+draw_x_angle()
+draw_random_move()
+draw_spirograph()
+
+
 
 
 screen = t.Screen()
